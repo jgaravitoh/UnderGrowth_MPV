0ffb113acaee52c48e8be316d0d8287864cbcfad
jgaravitoh <jgaravitoh@unal.edu.co>
2023-11-11 08:55:14 -0500

    Adds visual feedback to the selection system.

---
+++ Assets/Scripts/OutlineSelection.cs
@@ -0,0 +1,90 @@
+//MIT License
+//Copyright (c) 2023 DA LAB (https://www.youtube.com/@DA-LAB)
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+// The code used for this functionality was modified from an original code written
+// by DA LAB (https://github.com/DA-LAB-Tutorials/YouTube-Unity-Tutorials/blob/main/OutlineSelection.cs)
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OutlineSelection : MonoBehaviour
+{
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    // Array of tags that work with the selected system
+    private string[] allowedTags = { "Player", "Enemy", "Chest", "Door", "Meter las etiquetas necesarias" };
+
+    void Update()
+    {
+        // Highlight
+        if (highlight != null)
+        {
+            highlight.gameObject.GetComponent<Outline>().enabled = false;
+            highlight = null;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) // Event System MUST be in the editor hierarchy
+        {
+            highlight = raycastHit.transform;
+            if (System.Array.Exists(allowedTags, tag => tag == highlight.tag) && highlight != selection)
+            {
+                if (highlight.gameObject.GetComponent<Outline>() != null)
+                {
+                    highlight.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    Outline outline = highlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = new Color(0,0.588f,1,1);
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 5.0f;
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
+
+        // Selection
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (highlight)
+            {
+                if (selection != null)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                }
+                selection = raycastHit.transform;
+                selection.gameObject.GetComponent<Outline>().enabled = true;
+                highlight = null;
+            }
+            else
+            {
+                if (selection)
+                {
+                    selection.gameObject.GetComponent<Outline>().enabled = false;
+                    selection = null;
+                }
+            }
+        }
+    }
+}
+
