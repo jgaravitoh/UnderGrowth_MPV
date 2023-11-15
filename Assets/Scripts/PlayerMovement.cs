b4f921762c40c325592b596c6188f22eb08ff559
CrookedSpecs <saparadaa@unal.edu.co>
2023-11-14 21:17:30 -0500

    movimiento y seguimiento de camara

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement: MonoBehaviour
 {
+    [SerializeField] Transform target;
+
     public float velocidad = 5f;
     // This code sucks ass XD, we need to better it.
     void Update()
     {
-        // Obtener la entrada del teclado
-        float movimientoHorizontal = Input.GetAxis("Horizontal");
-        float movimientoVertical = Input.GetAxis("Vertical");
-
-        // Calcular la dirección del movimiento
-        Vector3 direccion = new Vector3(movimientoHorizontal, 0f, movimientoVertical).normalized;
-
-        // Mover el personaje
-        transform.Translate(direccion * velocidad * Time.deltaTime, Space.World);
+        //RayCasting
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+            //movement
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
